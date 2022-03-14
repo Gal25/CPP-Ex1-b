01640e3d2c55d71543fdca29697e6094e60eefc6
Gal25 <92460450+Gal25@users.noreply.github.com>
2022-03-14 18:32:19 +0200

    Update mat.hpp

--- mat.hpp
+++ mat.hpp
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <iostream>
 #include <string>
+#include <vector>
 
-//header file
 
 using namespace std;
 namespace ariel{
     string mat(int col, int row, char x, char y); //A function that builds the matrix according to the requirements
+    string toString(int r , int c,vector<vector<char>> newMat ); // A function that put the vector in to string
 }
